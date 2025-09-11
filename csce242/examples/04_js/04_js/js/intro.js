f16673d37573afc21dd9867d495d42bb1bdbcbf8
StephonJ <stephonj@email.sc.edu>
2025-09-11 12:12:30 -0400

    Json example

--- csce242/examples/04_js/04_js/js/intro.js
+++ csce242/examples/04_js/04_js/js/intro.js
@@ -5,6 +5,32 @@
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world");
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    document.getElementById("btn-click-me").classList.add("clicked");
+    event.currentTarget.classList.add("clicked"); //current target is the button that was clicked
+};
+
+document.getElementById("happy-click-me").onclick = (event) => {
+    document.getElementById("p-yay").innerHTML = "Yay!";
+    document.getElementById("happy-click-me").classList.add("happyclicked");
+    event.currentTarget.classList.add("happyclicked"); //current target is the button that was clicked
+};
+
+document.getElementById("sad-click-me").onclick = (event) => {
+    document.getElementById("p-nay").innerHTML = "Nay!";
+    document.getElementById("sad-click-me").classList.add("sadclicked");
+    event.currentTarget.classList.add("sadclicked"); //current target is the button that was clicked
+};
+
+document.getElementById("clear-click-me").onclick = (event) => {
+    document.getElementById("p-clear").innerHTML = " ";
+    document.getElementById("clear-click-me").classList.add("clearclicked");
+    event.currentTarget.classList.add("clearclicked"); //current target is the button that was clicked
+};
+
+document.getElementById("btn-happy").onclick = (event) => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Yay";
+    pFeeling.classList.add("happy");
 };
