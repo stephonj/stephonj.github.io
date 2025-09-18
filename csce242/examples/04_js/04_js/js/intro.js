aa1f6ba839879cd8a559c9355c782c20bd6d8660
StephonJ <stephonj@email.sc.edu>
2025-09-18 11:47:16 -0400

    Example Js

--- csce242/examples/04_js/04_js/js/intro.js
+++ csce242/examples/04_js/04_js/js/intro.js
@@ -1,4 +1,5 @@
-/*const sayHello = () => {
+/*
+const sayHello = () => {
     console.log("Hello World");
 }
 
@@ -7,33 +8,71 @@ document.getElementById("btn-click-me").onclick = sayHello;
 
 document.getElementById("btn-click-me").onclick = (event) => {
     document.getElementById("p-welcome").innerHTML = "Hello World";
-    document.getElementById("btn-click-me").classList.add("clicked");
-    event.currentTarget.classList.add("clicked"); //current target is the button that was clicked
+    event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
 };
 
 document.getElementById("btn-happy").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
-    pFeeling.innerHTML = "Yay";
+    pFeeling.innerHTML = "YAY";
     pFeeling.classList.add("happy");
-    pFeeling.classList.remove("happy");
-};
+    pFeeling.classList.remove("sad");
+}
 
 document.getElementById("btn-sad").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
     pFeeling.innerHTML = "Nay";
     pFeeling.classList.add("sad");
     pFeeling.classList.remove("happy");
-};
+}
 
 document.getElementById("btn-clear").onclick = () => {
     const pFeeling = document.getElementById("p-feeling");
     pFeeling.innerHTML = "";
     pFeeling.classList.remove("sad");
     pFeeling.classList.remove("happy");
-};
+}
 
 document.getElementById("txt-emotion").onkeyup = (event) => {
     const userInput = event.currentTarget.value;
     document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`
     document.getElementById("img-emotion").classList.remove("hidden");
+};
+
+document.getElementById("btn-mood-ring").onclick = () => {
+    const color = document.getElementById("txt-color").value.trim().toLowerCase();
+    const p = document.getElementById("p-mood-result");
+    p.innerHTML = "";   //reset paragraph
+    const error = document.getElementById("error-color");
+    error.innerHTML = "";  //reset error
+    let mood = "";
+    
+    if(color == ""){
+        error.innerHTML = "* blank";
+        return;
+    }
+
+    if(color == "red") {
+        mood = "angry";
+    } else if(color == "yellow") {
+        mood = "mellow";
+    }
+
+    if(mood == ""){
+        error.innerHTML = "* Invalid color";
+        return;
+    }
+
+    p.innerHTML = `You choose ${color}, so you are feeling ${mood}`;
+}
+
+document.getElementById("btn-bounce").onclick = (event) => {
+    const ball = document.getElementById("ball");
+
+    if(ball.classList.contains("bounce")){
+        event.currentTarget.innerHTML = "Start";
+    } else {
+        event.currentTarget.innerHTML = "Stop";
+    }
+
+    ball.classList.toggle("bounce");
 }
