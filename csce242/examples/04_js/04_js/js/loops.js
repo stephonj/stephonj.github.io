c27478c91c9302ba7595aa6f579a3cec18078966
StephonJ <stephonj@email.sc.edu>
2025-09-25 12:19:22 -0400

    Inclass Loops Example

--- csce242/examples/04_js/04_js/js/loops.js
+++ csce242/examples/04_js/04_js/js/loops.js
@@ -25,4 +25,51 @@ document.getElementById("btn-count").onclick = () => {
         error.innerHTML = "* The second number must be bigger than the first";
         return;
     }
+
+    //Loop for ever number from start to end display the number in paragraphs
+    for(let i=startNum; i <= endNum; i++){
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        resultDiv.append(p);
+        p.onclick = () => {
+            console.log(`You clicked Number ${i}`);
+        };
+    }
 };
+
+document.getElementById("btn-show-toys").onclick = () => {
+    const toyList = document.getElementById("toy-list");
+    toyList.innerHTML = "";
+
+    const toys = ["ball", "skipping rope", "doll", "mini car", "elmo"];
+
+    /* First way to iterate */
+    //interate over toys  add li's to the ul
+   /* for(let i=0; i < toys.length; i++){
+        const li = document.createElement("li");
+        li.innerHTML = toys[i];
+        toyList.append(li);
+    }*/
+
+    /* Second way preferred */
+    toys.forEach((toy, i)=>{
+        const li = document.createElement("li");
+        li.innerHTML = toy;
+        toyList.append(li);
+    });
+}
+
+document.getElementById("btn-show-toy-prices").onclick = () => {
+    const toys = []
+    toys["ball"] = 2.99;
+    toys["barbie"] = 19.99;
+    toys["skipping rope"] = 1.98;
+    toys["match box car"] = 2.54;
+
+    const toyDiv = document.getElementById("toy-prices");
+    toyDiv.innerHTML="";
+
+    for(let toy in toys) {
+        
+    }
+}
